b0b9a0383797343078a96acbac0ded15e77a07de
closure <126289616+closuretext@users.noreply.github.com>
2026-09-02 14:01:22 -0300

    - Delay

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function getST() {
 
 // Startup
 jQuery(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // So Recast loads first.
     try {
         const settingsHtml = await $.get(`${extensionFolderPath}/index.html`);
         const tempDiv = $("<div>").html(settingsHtml);
